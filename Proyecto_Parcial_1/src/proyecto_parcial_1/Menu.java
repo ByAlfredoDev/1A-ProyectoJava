be52538d8ea63eef9b25344237178d1c5756a959
ByAlfredoDev <anavarro3106@uta.edu.ec>
2026-04-09 22:34:09 -0500

    Switches agregados

---
+++ Proyecto_Parcial_1/src/proyecto_parcial_1/Menu.java
@@ -0,0 +1,28 @@
+package proyecto_parcial_1;
+
+public class Menu {
+    public void pantallaString() {
+        System.out.println("=== Menú ===");
+        System.out.println("1. Opción 1");
+        System.out.println("2. Opción 2");
+        System.out.println("3. Opción 3");
+        System.out.println("4. Opción 4");
+        System.out.println();
+    }
+    
+    public void opcion1() {
+        System.out.println("Elegiste opción 1");
+    }
+    
+    public void opcion2() {
+        System.out.println("Elegiste opción 2");
+    }
+    
+    public void opcion3() {
+        System.out.println("Elegiste opción 3");
+    }
+    
+    public void opcion4() {
+        System.out.println("Elegiste opción 4");
+    }
+}
--- Proyecto_Parcial_1/src/proyecto_parcial_1/Proyecto_Parcial_1.java
+++ Proyecto_Parcial_1/src/proyecto_parcial_1/Proyecto_Parcial_1.java
@@ -4,6 +4,8 @@
  */
 package proyecto_parcial_1;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Alfredo_EC
@@ -14,7 +16,31 @@ public class Proyecto_Parcial_1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        String metodo;
+        Scanner scan = new Scanner(System.in);
+        String opt;
+
+        Menu menu = new Menu();
+        do {
+            menu.pantallaString();
+            System.out.print("Ingresa una opción: ");
+            opt = scan.nextLine();
+            switch (opt) {
+                case "1" -> {
+                    menu.opcion1();
+                }
+                case "2" -> {
+                    menu.opcion2();
+                }
+                case "3" -> {
+                    menu.opcion3();
+                }
+                case "4" -> {
+                    menu.opcion4();
+                }
+                default -> {
+                    System.out.println("Opción no válida");
+                }
+            }
+        } while (true);
     }
-    
 }
