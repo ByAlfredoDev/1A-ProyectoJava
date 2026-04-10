dcf46f7de01fa9727d6ea3cd9dc2b4a422361f97
ByAlfredoDev <anavarro3106@uta.edu.ec>
2026-04-09 22:01:20 -0500

    Add variable declaration in main method

--- Proyecto_Parcial_1/src/proyecto_parcial_1/Proyecto_Parcial_1.java
+++ Proyecto_Parcial_1/src/proyecto_parcial_1/Proyecto_Parcial_1.java
@@ -14,7 +14,7 @@ public class Proyecto_Parcial_1 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        String metodo;
     }
     
 }
